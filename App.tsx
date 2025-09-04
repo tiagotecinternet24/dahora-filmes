11a68636caa6379ca8f5dbc04722aa1890cc515d
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 12:00:34 -0300

    Aplica icone ao pressable

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { StatusBar } from "expo-status-bar";
 import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+// Importando a biblioteca interna vector-icons
+import { Ionicons } from "@expo/vector-icons";
+
 export default function App() {
   /* Usamos o useFonts para criar uma referência para 
   a fonte que queremos utilizar no app. */
@@ -27,6 +30,7 @@ export default function App() {
         </View>
         <View style={estilos.viewBotoes}>
           <Pressable style={estilos.botaoInicial}>
+            <Ionicons name="search" size={24} color="white" />
             <Text style={estilos.textoBotao}>Buscar Filmes</Text>
           </Pressable>
 
@@ -74,6 +78,8 @@ const estilos = StyleSheet.create({
     backgroundColor: "#5451a6",
     padding: 16,
     borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
   },
   textoBotao: {
     color: "white",
