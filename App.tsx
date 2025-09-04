87524a65a07f63cb8cd35b94dc6bbb603ec1ae01
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 11:51:30 -0300

    Substitui Button por Pressable

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -26,7 +26,10 @@ export default function App() {
           <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Button color="red" title="Buscar Filmes" />
+          <Pressable style={estilos.botaoInicial}>
+            <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+          </Pressable>
+
           <Button title="Favoritos" />
         </View>
         <View style={estilos.viewRodape}>
@@ -66,6 +69,14 @@ const estilos = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
+  },
+  botaoInicial: {
+    backgroundColor: "#5451a6",
+    padding: 16,
+    borderRadius: 8,
+  },
+  textoBotao: {
+    color: "white",
   },
   viewRodape: {
     backgroundColor: "#ef9a9a",
