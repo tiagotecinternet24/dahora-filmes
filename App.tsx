a0d093db3ff6dab715d28f3800c76c76e0acd8f9
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 11:32:11 -0300

    Carrega fonte com useFonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  /* Usamos o useFonts para criar uma referência para 
+  a fonte que queremos utilizar no app. */
+  const [fonteCarregada] = useFonts({
+    Monoton: require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  // Se a fonte ainda não foi carregada
+  if (!fonteCarregada) {
+    return (
+      <Text style={{ color: "red", fontSize: 22 }}>Carregando fonte...</Text>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={estilos.container}>
