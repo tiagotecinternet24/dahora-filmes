263225a054682b65bf423e04bf24c1b698daf936
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 09:58:14 -0300

    Aplica proporções usando flex

--- App.tsx
+++ App.tsx
@@ -1,16 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ backgroundColor: "lightgreen", padding: 40 }}>
+      <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
-        <View>
+        <View style={estilos.viewLogo}>
           <Text>Dá Hora Filmes</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Text>Botão 1</Text>
+          <Text>Botão 2</Text>
+        </View>
+        <View style={estilos.viewRodape}>
+          <Text>Botão 3</Text>
+          <Text>Botão 4</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+/* Estilos */
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff9c4",
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  viewLogo: {
+    backgroundColor: "lightgreen",
+    flex: 3,
+  },
+  viewBotoes: {
+    backgroundColor: "#ffcc80",
+    flex: 2,
+  },
+  viewRodape: {
+    backgroundColor: "#ef9a9a",
+    flex: 0.5,
+  },
+});
