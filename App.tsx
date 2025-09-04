3198ff724bf344448c6d45ac3d7cdee2acf3cfa4
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 11:42:28 -0300

    Estiliza titulo do App

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ export default function App() {
         <StatusBar style="auto" />
         <View style={estilos.viewLogo}>
           <Image source={require("./assets/dahora.png")} style={estilos.logo} />
-          <Text>Dá Hora Filmes</Text>
+          <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
           <Button color="red" title="Buscar Filmes" />
@@ -54,6 +54,11 @@ const estilos = StyleSheet.create({
   logo: {
     width: 128,
     height: 128,
+  },
+  tituloApp: {
+    fontSize: 32,
+    color: "#5451a6",
+    fontFamily: "Monoton",
   },
   viewBotoes: {
     backgroundColor: "#ffcc80",
