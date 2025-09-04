b2c910fd85cdac3376687d48ab6aa8a97048ac51
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 10:47:11 -0300

    Adiciona componente nativo Button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -11,12 +11,12 @@ export default function App() {
           <Text>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Text>Botão 1</Text>
-          <Text>Botão 2</Text>
+          <Button color="red" title="Buscar Filmes" />
+          <Button title="Favoritos" />
         </View>
         <View style={estilos.viewRodape}>
-          <Text>Botão 3</Text>
-          <Text>Botão 4</Text>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
