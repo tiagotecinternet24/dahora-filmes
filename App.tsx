4e07a469afd40d4be077086803777212f1156657
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-04 11:11:42 -0300

    Adiciona imagem de logo

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -8,6 +8,7 @@ export default function App() {
       <SafeAreaView style={estilos.container}>
         <StatusBar style="auto" />
         <View style={estilos.viewLogo}>
+          <Image source={require("./assets/dahora.png")} style={estilos.logo} />
           <Text>Dá Hora Filmes</Text>
         </View>
         <View style={estilos.viewBotoes}>
@@ -26,16 +27,19 @@ export default function App() {
 /* Estilos */
 const estilos = StyleSheet.create({
   container: {
-    backgroundColor: "#fff9c4",
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
     flex: 1,
     justifyContent: "center",
   },
   viewLogo: {
-    backgroundColor: "lightgreen",
     flex: 3,
-    justifyContent: "flex-end",
+    justifyContent: "center",
     alignItems: "center",
+  },
+  logo: {
+    width: 128,
+    height: 128,
   },
   viewBotoes: {
     backgroundColor: "#ffcc80",
