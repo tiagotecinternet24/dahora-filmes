2ed7f88995ad9636d9d62727ee93b728bf32c3a2
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-18 09:20:33 -0300

    Migra config de font para layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
+import { useFonts } from "expo-font";
 import { View, Text } from "react-native";
+import Loading from "../src/components/Loading";
 
 export default function Layout() {
+  const [fonteCarregada] = useFonts({
+    Monoton: require("../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  if (!fonteCarregada) {
+    return <Loading />;
+  }
+
   return (
     <View>
       <Text>_layout</Text>
