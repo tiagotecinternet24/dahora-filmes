cb0172d53ff8fb1768caf1a928dfaf30b2cf6f63
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-18 09:32:20 -0300

    Configura Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { useFonts } from "expo-font";
-import { View, Text } from "react-native";
 import Loading from "../src/components/Loading";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Stack } from "expo-router";
 
 export default function Layout() {
   const [fonteCarregada] = useFonts({
@@ -12,8 +14,20 @@ export default function Layout() {
   }
 
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#5451a6",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </SafeAreaProvider>
   );
 }
