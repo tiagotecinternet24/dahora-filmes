d1ecab3aa01af4eae33feecffe11eec3c15c4f9a
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-18 09:08:10 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 // Importando a biblioteca interna vector-icons
 
+/* Usamos o @ts-ignore para ignorar erros de tipagem (ou ausência de tipagem) */
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import Loading from "./src/components/Loading";
