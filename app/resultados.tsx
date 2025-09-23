877a16609d1366ed7d5ce178c7314ea2bfbe551b
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-23 12:01:29 -0300

    Aplica useEffect e axios

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -3,9 +3,29 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { ParametrosBusca } from "../src/types";
+import { useEffect } from "react";
+import { api } from "../src/services/api";
 
 export default function Resultados() {
   const { filme } = useLocalSearchParams<ParametrosBusca>();
+
+  /* Criando a lógica para acesso ao serviço (API)
+  usando o axios */
+  useEffect(() => {
+    // Se não houver um filme definido, para tudo
+    if (!filme) return;
+
+    api
+      .get("/search/movie", {
+        params: {
+          language: "pt-BR",
+          query: filme,
+          include_adult: false,
+        },
+      })
+      .then((resposta) => console.log(resposta.data.results))
+      .catch((err) => console.error(err));
+  });
 
   return (
     <>
