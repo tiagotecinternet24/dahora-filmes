befd89f4099f827934d0bc2f2ec9389b7788fb2d
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-23 09:01:58 -0300

    Adiciona state e função para buscarFilme

--- app/busca.tsx
+++ app/busca.tsx
@@ -1,12 +1,22 @@
 // app/busca.tsx
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function Busca() {
+  // Hook para armazenar o filme digitado pelo usuário
+  const [filme, setFilme] = useState<string>("");
+
+  const buscarFilmes = () => {
+    if (!filme) {
+      return Alert.alert("Ops!", "Você deve digitar um filme!");
+    }
+  };
+
   return (
     <>
       <Stack.Screen
@@ -27,9 +37,12 @@ export default function Busca() {
             placeholder="Digite o nome do filme"
             style={estilos.campo}
             inputMode="search"
+            value={filme}
+            onChangeText={setFilme}
+            onSubmitEditing={buscarFilmes}
           />
         </View>
-        <Button title="Procurar" color="#5451a6" />
+        <Button title="Procurar" color="#5451a6" onPress={buscarFilmes} />
       </SafeAreaView>
     </>
   );
