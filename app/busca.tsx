02ee469a64c530a233c4687ebf69582908c993a0
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-23 08:32:19 -0300

    Cria tela busca básica

---
+++ app/busca.tsx
@@ -0,0 +1,55 @@
+// app/busca.tsx
+import { Button, StyleSheet, Text, View } from "react-native";
+
+// @ts-ignore
+import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Busca() {
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: "Busca de Filmes",
+        }}
+      />
+      <SafeAreaView style={estilos.container}>
+        <Text style={estilos.texto}>
+          Star Trek? O Poderoso Chefão? A trilogia Senhor dos Anéis?
+        </Text>
+        <Text style={estilos.texto}>
+          Localiza um filme que você viu ou gostaria de ver!
+        </Text>
+        <View style={estilos.viewForm}>
+          <Ionicons name="film" size={44} />
+        </View>
+        <Button title="Procurar" color="#5451a6" />
+      </SafeAreaView>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  texto: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  viewForm: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 16,
+  },
+  campo: {
+    borderWidth: 1,
+    padding: 8,
+    flex: 0.9,
+    borderRadius: 5,
+    borderColor: "#ccc",
+  },
+});
