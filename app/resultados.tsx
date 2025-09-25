1883ca9e585feaca9b187e7ed1c8508de6d4a6d4
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-25 09:16:49 -0300

    Adiciona states para resultados e loading

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -2,18 +2,24 @@
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { ParametrosBusca } from "@/src/types";
-import { useEffect } from "react";
+import { Filme, ParametrosBusca } from "@/src/types";
+import { useEffect, useState } from "react";
 import { api } from "@/src/services/api";
 
 export default function Resultados() {
   const { filme } = useLocalSearchParams<ParametrosBusca>();
 
-  /* Criando a lógica para acesso ao serviço (API)
-  usando o axios */
+  // Criando um state para gerenciar a lista de filmes obtida da API
+  const [resultados, setResultados] = useState<Filme[]>([]);
+
+  // Criando um state para alternar a exibição de um Loading
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    // Se não houver um filme definido, para tudo
     if (!filme) return;
+
+    // Ao começar as ações de busca na API, iniciamos o loading
+    setLoading(true);
 
     api
       .get("/search/movie", {
@@ -23,9 +29,13 @@ export default function Resultados() {
           include_adult: false,
         },
       })
-      .then((resposta) => console.log(resposta.data.results))
-      .catch((err) => console.error(err));
-  });
+      .then((resposta) => setResultados(resposta.data.results))
+      .catch((err) => console.error(err))
+
+      // Acabou o processo de busca? Mesmo com sucesso ou erro?
+      // Então, finalmente, desative o loading
+      .finally(() => setLoading(false));
+  }, [filme]);
 
   return (
     <>
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,10 @@
 export type ParametrosBusca = {
   filme: string;
 };
+
+// Tipo para os dados de filme que vêm da API
+export type Filme = {
+  id: number;
+  title: string;
+  poster_path: string;
+};
