cf1b6ceb22363e938e4c347823be741a3c8b7ca8
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-26 12:00:11 -0300

    Adiciona função buscarFavoritos

--- src/services/storage-favoritos.ts
+++ src/services/storage-favoritos.ts
@@ -62,3 +62,10 @@ export async function salvarFilmeFavorito(filme: Filme): Promise<boolean> {
   // Retorna true sinalizando que o filme foi salvo
   return true;
 }
+
+/**
+ * Chama a função carregar e retorna a lista de favoritos (filmes)
+ */
+export async function buscarFavoritos(): Promise<Filme[]> {
+  return carregar();
+}
