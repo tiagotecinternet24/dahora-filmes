d5571fa1d630e5fb3444ff6e268c6b394a739b6c
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-26 12:00:32 -0300

    Adiciona useEffect para carregando favoritos

--- app/favoritos.tsx
+++ app/favoritos.tsx
@@ -5,8 +5,25 @@ import { Stack } from "expo-router";
 
 // @ts-ignore
 import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { Filme } from "@/src/types";
+import { buscarFavoritos } from "@/src/services/storage-favoritos";
 
 export default function Favoritos() {
+  const [favoritos, setFavoritos] = useState<Filme[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    buscarFavoritos()
+      .then((lista) => {
+        setFavoritos(lista);
+      })
+      .catch((erro) => console.error("Erro ao carregar os filmes:" + erro))
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(favoritos);
+
   const itemDaListaDeFavoritos = () => (
     <Pressable style={estilos.item}>
       <Text style={estilos.titulo}>Titulo...</Text>
