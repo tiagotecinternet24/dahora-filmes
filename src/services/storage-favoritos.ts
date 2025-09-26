11292a86d158b92ca27133c1a0dfa465ef732f75
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-26 11:15:14 -0300

    Cria carregar, salvarLista e salvarFilmeFavorito
    Funções em service/storage-favoritos.ts

---
+++ src/services/storage-favoritos.ts
@@ -0,0 +1,64 @@
+// src/services/storage-favoritos.ts
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Filme } from "../types";
+
+// Criando uma identificação para a área de favoritos no dispositivo
+const STORAGE_FAVORITOS = "filmes_favoritos";
+
+/**
+ * Lê a lista de favoritos do Async Storage
+ * Retorna um array vazio em caso de erro ou chave inexistente
+ */
+async function carregar(): Promise<Filme[]> {
+  try {
+    // Lê a string salva no storage (pode vir null)
+    const favoritosArmazenados = await AsyncStorage.getItem(STORAGE_FAVORITOS);
+
+    // Havendo favoritosArmazenados (como string), retorna convertido para objeto.
+    // Senão, retorna um array vazio
+    return favoritosArmazenados ? JSON.parse(favoritosArmazenados) : [];
+  } catch (error) {
+    console.error("Erro ao ler storage:" + error);
+    return [];
+  }
+}
+
+/**
+ * Grava a lista de favoritos no Async Storage
+ */
+async function salvarLista(lista: Filme[]): Promise<void> {
+  try {
+    // Salva a lista de filmes no storage em formato de string
+    await AsyncStorage.setItem(STORAGE_FAVORITOS, JSON.stringify(lista));
+  } catch (error) {
+    console.error("Erro ao salvar a lista no storage: " + error);
+  }
+}
+
+/**
+ * Salvar um filme na lista de favoritos (e no próprio storage)
+ * Retorna true se salvou com sucesso, false se o filme já estava nos favoritos
+ */
+async function salvarFilmeFavorito(filme: Filme): Promise<boolean> {
+  const favoritos = await carregar();
+
+  /* Verifica se um filme já existe com o mesmo id na lista/storage
+   de favoritos. A função some() retorna true se encontrar pelo menos
+   1 item que satisfaça a condição. */
+  if (favoritos.some((filmeExistente) => filmeExistente.id === filme.id)) {
+    // Já existe? Não precisamos salvar de novo
+    // Por isso, retornamos false para indicar que o filme NÃO FOI adicionado
+    return false;
+  }
+
+  // Se chegou até aqui, é porque esse filme ainda não tinha sido salvo.
+  // Portanto, adicionamos à lista de favoritos
+  favoritos.push(filme);
+
+  // Salva a lista atualizada no storage
+  await salvarLista(favoritos);
+
+  // Retorna true sinalizando que o filme foi salvo
+  return true;
+}
