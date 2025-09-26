ddf58542aab6b505978c1f3e10a6887e0a609c38
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-26 09:40:12 -0300

    Novos types para param. Detalhe e FilmeDetalhes

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,18 @@ export type Filme = {
 export type CardFilmeProps = {
   filme: Filme;
 };
+
+export type ParametrosDetalhes = {
+  id: string;
+  filme: string;
+};
+
+// Tipo para os dados detalhados do filme da API
+export type FilmeDetalhes = {
+  id: number;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  backdrop_path: string | null;
+};
