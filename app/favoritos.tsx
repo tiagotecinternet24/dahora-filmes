1b03490c2af580d4bad5a9a0b1e7c484cb0ce7cd
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-30 08:58:01 -0300

    Configura botões do Alert

--- app/favoritos.tsx
+++ app/favoritos.tsx
@@ -15,6 +15,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
 import { Filme, ItemDaListaDeFavoritosProps } from "@/src/types";
 import {
+  apagarTodosFavoritos,
   buscarFavoritos,
   removerFilmeFavorito,
 } from "@/src/services/storage-favoritos";
@@ -86,7 +87,22 @@ export default function Favoritos() {
   const apagarTudo = () => {
     Alert.alert(
       "❗Apagar todos os favoritos",
-      "Tem certeza que deseja apagar todos os filmes favoritos?"
+      "Tem certeza que deseja apagar todos os filmes favoritos?",
+      [
+        { text: "Não", style: "cancel" },
+        {
+          text: "Sim",
+          onPress: async () => {
+            try {
+              await apagarTodosFavoritos();
+              setFavoritos([]);
+            } catch (error) {
+              console.error(error);
+              Alert.alert("Erro", "Não foi possível apagar os favoritos");
+            }
+          },
+        },
+      ]
     );
   };
 
