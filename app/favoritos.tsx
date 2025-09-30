9323cee8c081bfa8fa4ecaf82b612b48e697891f
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-09-30 08:40:35 -0300

    Adiciona função apagarTudo básica

--- app/favoritos.tsx
+++ app/favoritos.tsx
@@ -83,6 +83,13 @@ export default function Favoritos() {
     }
   };
 
+  const apagarTudo = () => {
+    Alert.alert(
+      "❗Apagar todos os favoritos",
+      "Tem certeza que deseja apagar todos os filmes favoritos?"
+    );
+  };
+
   return (
     <>
       <Stack.Screen
