2ecc5e33aabf1567159d086b446eeaeba4efef8b
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-10-02 10:14:54 -0300

    Adiciona ID baseado em dominio

--- app.config.ts
+++ app.config.ts
@@ -29,6 +29,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#000000",
     },
     edgeToEdgeEnabled: true,
+    package: "com.tiagohub.dahorafilmes",
   },
   plugins: ["expo-font", "expo-router"],
   // Acessando a API_KEY através do dotenv e guardando na chave apiKey
